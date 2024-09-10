e43003a9224304a9a111aba5668024308a376805
yoo94 <ahy0119@naver.com>
2024-09-11 01:18:13 +0900

    -feat: Framer Motion 적용

--- src/app/(default)/components/header.tsx
+++ src/app/(default)/components/header.tsx
@@ -1,22 +1,31 @@
+'use client'
 import React from "react";
 import { HeartPulse, SquareMenu } from "lucide-react";
+import { motion, useAnimationControls } from "framer-motion"
 
 const Header = () => {
   const SubMenuNames = ["Carrer", "Group", "Personal", "Skill"];
-
+  const menuControl = useAnimationControls();
+  
+  const openMenuAnimation = () => {
+      menuControl.start({opacity:0, height:0});
+  }
   return (
     <div className="flex justify-between items-center bg-black text-white px-1.5">
       <div className="flex items-center pl-5">
         <HeartPulse color="red" />
       </div>
-
       <div className="flex-grow flex justify-center">
         <nav className="flex items-center">
           <div className="max-w-screen-lg mx-auto px-4">
             <ul className="flex items-center text-xs justify-center gap-20">
               {SubMenuNames.map((item, index) => (
                 <li key={index}>
-                  <a className="hidden min-[835px]:flex text-[#ceccce] hover:text-white font-bold p-3 pr-5 cursor-pointer">
+                  <a 
+                  key={index}
+                  className="hidden min-[835px]:flex text-[#ceccce] hover:text-white font-bold p-3 pr-5 cursor-pointer"
+                  onMouseEnter={()=>{openMenuAnimation()}}
+                  >
                     {item}
                   </a>
                 </li>
@@ -27,6 +36,17 @@ const Header = () => {
               </div>
           </div>
         </nav>
+        <div className="bg-[#ldldlf] absolute w-full overflow-hidden left-0 top-0 z-10"
+          >
+            <h1>dafsasfsafdd</h1>
+        </div>
+        <motion.div className="bg-[#ldldlf] absolute w-full overflow-hidden left-0 top-0 z-10"
+          initial={{opacity:0, height:0}}
+          animate={menuControl}
+          transition={{duration:0.4, delayChildren:0.5}}
+          >
+            <h1>dafsasfsafdd</h1>
+        </motion.div>
       </div>
 
       <div className="flex items-center min-[835px]:hidden cursor-pointer p-3 pr-20">
