8284b8418f7431c94128e6051f510f3101c1109d
yoo94 <ahy0119@naver.com>
2024-09-11 00:46:54 +0900

    -feat: header 반응형 구현

--- src/app/(default)/components/header.tsx
+++ src/app/(default)/components/header.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import { HeartPulse, SquareMenu } from "lucide-react";
 
-export default function Header() {
+const Header = () => {
+  const SubMenuNames = ["Carrer", "Group", "Personal", "Skill"];
+
   return (
-    <div>
-      Header
+    <div className="flex justify-between items-center bg-black text-white px-1.5">
+      <div className="flex items-center pl-5">
+        <HeartPulse color="red" />
+      </div>
+
+      <div className="flex-grow flex justify-center">
+        <nav className="flex items-center">
+          <div className="max-w-screen-lg mx-auto px-4">
+            <ul className="flex items-center text-xs justify-center gap-20">
+              {SubMenuNames.map((item, index) => (
+                <li key={index}>
+                  <a className="hidden min-[835px]:flex text-[#ceccce] hover:text-white font-bold p-3 pr-5 cursor-pointer">
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <div className="text-2xl min-[835px]:hidden text-center text-white font-bold">
+                Project Profile
+              </div>
+          </div>
+        </nav>
+      </div>
+
+      <div className="flex items-center min-[835px]:hidden cursor-pointer p-3 pr-20">
+        <SquareMenu />
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Header;
--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import '@/app/globals.css';
 import React from 'react';
 import Header from '@/app/(default)/components/header';
-import Footer from '@/app/(default)/components/footer';
 
 export const metadata: Metadata = {
   title: 'project', // title을 항상 문자열로 보장
@@ -23,7 +22,6 @@ export default function RootLayout({
       <main className="container justify-center p-4 xs:p-6 md:p-8 lg:p-12 xl:p-[100px]">
         {children}
       </main>
-      <Footer />
     </>
   );
 }
