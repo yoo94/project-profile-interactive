266990e4c10e051b092680456ac50a71d709d6f3
yoo94 <jaeseok9405@gmail.com>
2024-09-11 13:37:20 +0900

    -feat: 컴포넌트 분리

---
+++ src/app/(default)/components/backdrop.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { motion, AnimationControls } from 'framer-motion';
+
+interface BackdropProps {
+  backdropControl: AnimationControls;
+}
+
+const Backdrop: React.FC<BackdropProps> = ({ backdropControl }) => {
+  return (
+    <motion.div
+      className="fixed w-full left-0 z-[1] top-16"
+      initial={{ backdropFilter: "none", height: 0 }}
+      animate={backdropControl}
+      transition={{ duration: 0.2 }}
+    />
+  );
+};
+
+export default Backdrop;
