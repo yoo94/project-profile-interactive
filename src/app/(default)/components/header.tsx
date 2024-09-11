85b22829a897a8746ebe1b9dd28c34566effeae8
yoo94 <jaeseok9405@gmail.com>
2024-09-11 10:50:19 +0900

    -feat: submenu추가

--- src/app/(default)/components/header.tsx
+++ src/app/(default)/components/header.tsx
@@ -1,14 +1,76 @@
 'use client'
-import React from "react";
+import React, { useState } from "react";
 import { HeartPulse, SquareMenu } from "lucide-react";
 import { motion, useAnimationControls } from "framer-motion"
 
 const Header = () => {
   const SubMenuNames = ["Carrer", "Group", "Personal", "Skill"];
+  const [menuIdx, setMenuIdx] = useState(0);
+  const renderSubMenu = () => {
+    switch (menuIdx) {
+      case 0:
+        return (
+          <motion.div className="flex flex-row gap-14 justify-center w-full mx-auto text-center">
+            <div className="flex flex-col gap-3 ">
+              <h2>회사프로젝트</h2>
+              <ul className="flex flex-col gap-2 font-semibold text-[#eBeBed] text-2xl">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>3</li>
+              </ul>
+            </div>
+          </motion.div>
+        );
+      case 1:
+        return (
+          <motion.div className="w-full mx-auto text-center">
+            <div className="flex flex-col gap-3 ">
+              <h2>그룹프로젝트</h2>
+              <ul className="flex flex-col gap-2 font-semibold text-[#eBeBed] text-2xl">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </div>
+          </motion.div>
+        );
+      case 2:
+        return (
+          <motion.div className="w-full mx-auto text-center">
+            <div className="flex flex-col gap-3 ">
+              <h2>개인프로젝트</h2>
+              <ul className="flex flex-col gap-2 font-semibold text-[#eBeBed] text-2xl">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </div>
+          </motion.div>
+        );
+      case 3:
+        return (
+          <motion.div className="w-full mx-auto text-center">
+            <div className="flex flex-col gap-3 ">
+              <h2>기술</h2>
+              <ul className="flex flex-col gap-2 font-semibold text-[#eBeBed] text-2xl">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </div>
+          </motion.div>
+        );
+      default:
+        return <div className="text-2xl">coming soon</div>;
+    }
+  };
   const menuControl = useAnimationControls();
-  
   const openMenuAnimation = () => {
-      menuControl.start({opacity:0, height:0});
+    menuControl.start({ opacity: 1, height: "auto" });
+  }
+  const closeMenuAnimation = () => {
+    menuControl.start({ opacity: 0, height: 0 });
   }
   return (
     <div className="flex justify-between items-center bg-black text-white px-1.5">
@@ -21,10 +83,14 @@ const Header = () => {
             <ul className="flex items-center text-xs justify-center gap-20">
               {SubMenuNames.map((item, index) => (
                 <li key={index}>
-                  <a 
-                  key={index}
-                  className="hidden min-[835px]:flex text-[#ceccce] hover:text-white font-bold p-3 pr-5 cursor-pointer"
-                  onMouseEnter={()=>{openMenuAnimation()}}
+                  <a
+                    key={index}
+                    className="hidden min-[835px]:flex text-[#ceccce] hover:text-white font-bold p-3 pr-5 cursor-pointer text-base"
+                    onMouseEnter={() => {
+                      setMenuIdx(index);
+                      openMenuAnimation()
+                    }}
+                    onMouseLeave={() => { closeMenuAnimation() }}
                   >
                     {item}
                   </a>
@@ -32,20 +98,17 @@ const Header = () => {
               ))}
             </ul>
             <div className="text-2xl min-[835px]:hidden text-center text-white font-bold">
-                Project Profile
-              </div>
+              Project Profile
+            </div>
           </div>
         </nav>
-        <div className="bg-[#ldldlf] absolute w-full overflow-hidden left-0 top-0 z-10"
-          >
-            <h1>dafsasfsafdd</h1>
-        </div>
-        <motion.div className="bg-[#ldldlf] absolute w-full overflow-hidden left-0 top-0 z-10"
-          initial={{opacity:0, height:0}}
+        <motion.div className="bg-[#111112] absolute w-full overflow-hidden left-0 z-10 top-16
+        "
+          initial={{ opacity: 0, height: 0 }}
           animate={menuControl}
-          transition={{duration:0.4, delayChildren:0.5}}
-          >
-            <h1>dafsasfsafdd</h1>
+          transition={{ duration: 0.4, delayChildren: 0.5 }}
+        >
+          <div>{renderSubMenu()}</div>
         </motion.div>
       </div>
 
